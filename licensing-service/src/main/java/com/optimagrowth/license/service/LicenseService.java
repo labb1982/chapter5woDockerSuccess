9eabb724182d684ecfa154b8a92a38229d0bf043
lab <labb@rssoftware.co.in>
2021-06-23 20:57:09 +0530

    Added

---
+++ licensing-service/src/main/java/com/optimagrowth/license/service/LicenseService.java
@@ -0,0 +1,57 @@
+package com.optimagrowth.license.service;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Service;
+
+import com.optimagrowth.license.config.ServiceConfig;
+import com.optimagrowth.license.model.License;
+import com.optimagrowth.license.repository.LicenseRepository;
+
+@Service
+public class LicenseService {
+
+	@Autowired
+	MessageSource messages;
+
+	@Autowired
+	private LicenseRepository licenseRepository;
+
+	@Autowired
+	ServiceConfig config;
+
+
+	public License getLicense(final String licenseId, final String organizationId){
+		final License license = licenseRepository.findByOrganizationIdAndLicenseId(organizationId, licenseId);
+		if (null == license) {
+			throw new IllegalArgumentException(String.format(messages.getMessage("license.search.error.message", null, null),
+					config.getProperty(), licenseId, organizationId));	
+		}
+		return license.withComment(config.getProperty());
+	}
+
+	public License createLicense(final License license){
+		license.setLicenseId(UUID.randomUUID().toString());
+		licenseRepository.save(license);
+
+		return license.withComment(config.getProperty());
+	}
+
+	public License updateLicense(final License license){
+		licenseRepository.save(license);
+
+		return license.withComment(config.getProperty());
+	}
+
+	public String deleteLicense(final String licenseId){
+		String responseMessage = null;
+		final License license = new License();
+		license.setLicenseId(licenseId);
+		licenseRepository.delete(license);
+		responseMessage = String.format(messages.getMessage("license.delete.message", null, null),licenseId);
+		return responseMessage;
+
+	}
+}
